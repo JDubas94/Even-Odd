4b7b5501e59e19a14323ebd7486fcd9729f8d9f9
Julia Dubas <uliadubas94@gmail.com>
2022-07-30 13:29:40 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter a range of numbers!");
+
+            int a = int.Parse(Console.ReadLine());
+
+            int b = int.Parse(Console.ReadLine());
+
+            int i;
+
+            int countEvenNumder = 0, countOddNumber = 0;
+
+            int evenSumma = 0, oddSumma = 0;
+
+            if (a > b)
+            {
+                i = a;
+
+                a = b;
+
+                b = i;
+            }
+
+
+            while (a <= b)
+            {
+                if (a % 2 == 0)
+                {
+                    countEvenNumder++;
+                    evenSumma += a;
+                }
+                else
+                {
+                    countOddNumber++;
+                    oddSumma += a;
+
+                }
+                a++;
+            }
+           Console.WriteLine("Number of even: " + countEvenNumder);
+
+           Console.WriteLine("Number of odd: " + countOddNumber);
+
+            Console.WriteLine("Sum of even: " + evenSumma);
+
+            Console.WriteLine("Sum of odd: " + oddSumma);
+
+
+
+
+
+        }
+    }
+}
